a6e38aa0749cbf0a7132ae830a9ec68be000d339
Saidu45 <saiduth200@gmail.com>
2025-07-11 13:11:09 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MY FIRST REACT APP
+          MY FIRST REACT
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
